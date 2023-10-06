8c4ef5d7814b866dfc78e047f0f399e5ed94f761
shadeisroot <dschristensen@proton.me>
2023-10-06 11:16:45 +0200

    Deleted HelloApplication.java made a main

--- src/main/java/com/example/memoryproject/Main.java
+++ src/main/java/com/example/memoryproject/Main.java
@@ -2,16 +2,20 @@ package com.example.memoryproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage stage){
+        Group root = new Group();
+
+
+
+        Scene scene = new Scene(root,320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
