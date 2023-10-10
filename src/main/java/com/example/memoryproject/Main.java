1d13b7547d14072f376019ae4f0547db33e2d259
shadeisroot <dschristensen@proton.me>
2023-10-10 12:05:30 +0200

    Added a scene with a button

--- src/main/java/com/example/memoryproject/Main.java
+++ src/main/java/com/example/memoryproject/Main.java
@@ -3,34 +3,51 @@ package com.example.memoryproject;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     private Pane root;
-
+    private Pane introscene;
+    private WelcomeScreen ws;
 
     @Override
-    public void start(Stage stage){
+    public void start(Stage stage) {
+        introscene = new StackPane();
+        introscene.setPrefSize(1250, 1000);
+        Button btn = new Button("Start Game");
+        btn.setOnAction(e -> {
+            gameStart(stage);
+        });
+        btn.setScaleX(5);
+        btn.setScaleY(5);
+        introscene.getChildren().add(btn);
+
+        Scene intro = new Scene(introscene);
+        stage.setTitle("Welcome");
+        stage.setScene(intro);
+        stage.show();
+    }
+
+    public void gameStart(Stage stage){
+        MemeArray.shuffleMemes();
         root = new Pane();
         root.setPrefSize(1250, 1000);
-        MemeArray.shuffleMemes();
-
-       EventHandler<MouseEvent> eventHandler = event -> {
+        Scene scene = new Scene(root);
+        EventHandler<MouseEvent> eventHandler = event -> {
             Piece p = (Piece) event.getSource();
             p.flip();
         };
 
-        Scene scene = new Scene(root);
-
         stage.setTitle("MemoryGame");
         stage.setScene(scene);
         buildGame(eventHandler);
+
         stage.show();
-
     }
-
     public void buildGame(EventHandler<MouseEvent> eventHandler) {
         Piece[][] pieces = new Piece[5][4];
         int id = 1;
@@ -43,6 +60,7 @@ public class Main extends Application {
                 }
             }
     }
+
 
     public static void main(String[] args) {
         launch();
---
+++ src/main/java/com/example/memoryproject/WelcomeScreen.java
@@ -0,0 +1,19 @@
+package com.example.memoryproject;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class WelcomeScreen extends ImageView {
+    private Button btn;
+
+    public WelcomeScreen(){
+     //   mn = m;
+       // Button btn = new Button("Click mofo");
+      //  introscene.getChildren().add(btn);
+
+
+    }
+
+
+
+}
